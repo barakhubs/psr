e6a44884c31209caea13747b8cd3faa386363561
vitaja <j.candia@outlook.com>
2023-01-03 12:09:41 +0300

    fixed sms

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -20,7 +20,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('send:reminder')->daily('06:00');
+        $schedule->command('send:reminder')->everyTwoMinutes();
         $schedule->command('send:oneDayReminder')->dailyAt('06:00');
     }
 
